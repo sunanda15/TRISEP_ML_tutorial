f551b0a63ea47e7b5ba21f572705a2e793e6a708
sunanda15 <89693754+sunanda15@users.noreply.github.com>
2021-09-23 19:01:32 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,2 +1,2 @@
 dig +short myip.opendns.com @resolver1.opendns.com > ip.txt
-singularity exec --nv -B /fast_scratch/triumfmlutils/containers/base_ml_recommended.simg
+singularity exec --nv -B /fast_scratch/triumfmlutils/containers/base_ml_recommended.simg /bin/bash
